67b4a592eee08de60f5978067374734cbe8bfc3f
José Luis Chávez Flores <jose.chavez9027@alumnos.udg.mx>
2025-02-08 21:45:26 -0600

    VERSION2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
-import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Holi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Title from './components/title';
+import Input from './components/input';
+import TaskList from './components/task-list';
+import Layout from './components/layout';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTask: "",
+      tasks: []
+    };
+  }
+
+  handleTaskChange = event => {
+    this.setState({
+      newTask: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    let newTasks = this.state.tasks;
+    newTasks.push(this.state.newTask);
+    this.setState({
+      newTask: "",
+      tasks: newTasks
+    });
+  };
+
+  handleDelete = (id) => {
+    let newTasks = this.state.tasks;
+    newTasks.splice(id, 1)
+    this.setState({
+      tasks: newTasks,
+    })
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Title/>
+        <Input 
+          handleSubmit={this.handleSubmit}
+          handleTaskChange={this.handleTaskChange}
+          value={this.state.newTask}
+        />
+        <TaskList 
+          tasks={this.state.tasks}
+          handleDelete={this.handleDelete}
+        />
+      </Layout>
+    );
+  }
 }
 
 export default App;
